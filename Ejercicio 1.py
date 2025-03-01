4fd98993eb51bbba3ba107edc0f4cd33763133ee
Usuario <CORREO>
2025-03-01 13:48:55 +0100

    PYTHON

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,7 +1,6 @@
 def convertBinaryToDecimal(numeroBinario):
     # Asegurarse de que el número esté en str
     numeroTratar = str(numeroBinario)
-    
     # Convertir el número binario a decimal
     resultado = int(numeroTratar, 2)
     
